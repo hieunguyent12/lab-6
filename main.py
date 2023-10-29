cfe912342b8559cf3ded577076a2a4ea8c359f2a
Emily <emilyapel11@gmail.com>
2023-10-29 16:31:32 -0400

    i added the decoder and finished the main function

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ def encode(password):
 
     return result
 
-def decode():
-    pass
+def decode(encoded_password):
+    decoded_pass = ""
+    for dig in stored_encoded_password:
+        d = int(dig)
+        if (d - 3) < 0:
+            d = 10 + (d - 3)
+        else:
+            d = d - 3
+        decoded_pass += str(d)
+
+    return(decoded_pass)
 
 def main():
-
+    global stored_encoded_password
     option = None
 
     while option != 3:
@@ -33,13 +42,11 @@ def main():
             stored_encoded_password = encode(input("Please enter your password to encode: "))
             print("Your password has been encoded and stored!")
         elif option == 2:
-            decode()
+            original_pass = decode(stored_encoded_password)
+            print(f'The encoded password is {stored_encoded_password}, and the original password is {original_pass}.')
         elif option == 3:
             break
 
         print()
-
-
-
 
 main()
