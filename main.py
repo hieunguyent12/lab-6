879bde9acb7ec856bd77d8daa4ece6da398a3503
Hieu <hieunguyen.0527@gmail.com>
2023-10-29 15:18:58 -0400

    hi

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def encode():
-    pass
+def encode(password):
+    result = ""
+    for digit in password:
+        digit = int(digit)
+
+        new_digit = digit + 3
+        result += str(new_digit % 9 - 1 if new_digit > 9 else new_digit)
+
+    return result
+
 
 def main():
-    pass
+    a = encode(input("Enter a password: "))
+    print(a)
+
+main()
